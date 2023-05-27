cadf5c62e4705678c04da6d56d5327341e456bda
Bear-HZ <113594957+Bear-HZ@users.noreply.github.com>
2023-05-28 04:39:50 +0530

    Add files via upload

---
+++ NoWind.lua
@@ -0,0 +1,13 @@
+script_name("NoWind")
+script_description("Blocks servers from applying windy weather profiles")
+script_author("Bear")
+script_version("1.0.0")
+
+local sampev = require "lib.samp.events"
+local windyWeatherIds = {1, 3, 4, 5, 7, 8, 9, 10, 12, 14, 15, 16, 17, 18, 19, 21, 22, 31, 32, 42, 43, 44, 45}
+
+function sampev.onSetWeather(weatherId)
+	for _, windyWeatherId in pairs(windyWeatherIds) do
+		if weatherId == windyWeatherId then return false end
+	end
+end
